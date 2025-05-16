4b70da11e8b37e7365e3ca6693c0a5b83b8bcc67
Mohammad Reazaul Karim <info@apprain.com>
2025-05-16 00:27:36 -0400

    Vercel Code Update

--- server.js
+++ server.js
@@ -14,26 +14,17 @@
 
 
 require('dotenv').config();
-
 const express = require('express');
 const path = require("path");
 const app = express();
 const cors = require('cors');
 const SitesDB = require("./modules/sitesDB.js");
 const db = new SitesDB();
-//console.log(db);
-
-//const dataService = require('./data-service.js');
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors()); // Allow all origins
 app.use(express.json()); // To enable jost parser
-
-
-// Initialize Database
-
-
 
 app.get('/', (req, res) => {
     res.json({"message": "API Listening", "term": "Summer 2025", "student": "178417234"})
@@ -42,7 +33,6 @@ app.get('/', (req, res) => {
 
 app.post('/api/sites', async (req, res) => {
     try {
-        await db.ensureInitialized(process.env.MONGODB_CONN_STRING);
         const newSite = await db.addNewSite(req.body);
         res.status(201).json(newSite);
     } catch (err) {
@@ -53,15 +43,13 @@ app.post('/api/sites', async (req, res) => {
 
 app.get('/api/sites', async (req, res) => {
     try {
-       // await db.ensureInitialized(process.env.MONGODB_CONN_STRING);        
+   
         const page = parseInt(req.query.page) || 1;
         const perPage = parseInt(req.query.perPage) || 10;
-
         const filters = {};
         if (req.query.name) filters.name = req.query.name;
         if (req.query.region) filters.region = req.query.region;
         if (req.query.provinceOrTerritoryName) filters.provinceOrTerritoryName = req.query.provinceOrTerritoryName;
-        //name, region, provinceOrTerritoryName    
         const sites = await db.getAllSites(page, perPage, filters.name,filters.provinceOrTerritoryName);
         res.status(200).json(sites);
         
@@ -74,7 +62,6 @@ app.get('/api/sites', async (req, res) => {
 app.get('/api/sites/:id', async (req, res) => { 
     
     try {
-        //await db.ensureInitialized(process.env.MONGODB_CONN_STRING);
         const site = await db.getSiteById(req.params.id);
         
         if (!site) {
@@ -89,7 +76,6 @@ app.get('/api/sites/:id', async (req, res) => {
 
 app.put('/api/sites/:id', async (req, res) => {
     try {
-        await db.ensureInitialized(process.env.MONGODB_CONN_STRING);
         const updatedSite = await db.updateSiteById(req.body,req.params.id);
 
         if (!updatedSite) {
@@ -105,7 +91,6 @@ app.put('/api/sites/:id', async (req, res) => {
 
 app.delete('/api/sites/:id', async (req, res) => {
     try {
-         //await db.ensureInitialized(process.env.MONGODB_CONN_STRING);
         const success = await db.deleteSiteById(req.params.id);
         if (!success) {
             return res.status(404).json({ message: 'Site not found' });
@@ -115,8 +100,6 @@ app.delete('/api/sites/:id', async (req, res) => {
         res.status(500).json({ message: 'Error deleting site', error: err.message });
     }
 });
-
-
 
 // Resource not found (this should be at the end)
 app.use((req, res) => {
@@ -133,8 +116,3 @@ db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
     console.log(err);
 });
  
-
-//module.exports = app;
-
-// Below code is for call with our versel
-//app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
